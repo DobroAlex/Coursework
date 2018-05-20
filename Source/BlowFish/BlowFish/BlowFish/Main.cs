af03da8d0047f5579dcf36643ea83fd3ea11e1f9
Alex <alexkoshernosiegov@gmail.com>
2018-05-20 22:29:28 +0300

    IT S ALIVE

---
+++ Source/BlowFish/BlowFish/BlowFish/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BlowFishCS; 
+namespace Main
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Input key (up to 16 char):");
+            string key = Console.ReadLine();
+            if (key.Length>16 || key.Length==0)
+            {
+                Console.WriteLine("Key is empty or too long");
+                Environment.Exit(-1);
+            }
+            //BlowFish b = new BlowFish("04B915BA43FEB5B6");
+            BlowFish b = new BlowFish(key);
+            Console.WriteLine("Input line:");
+            string plainTxt=Console.ReadLine();
+            string chipherTxt = b.Encrypt_CBC(plainTxt);
+            Console.WriteLine("chipher text is :" + chipherTxt);
+            Console.WriteLine("Decrypted text is: "+ b.Decrypt_CBC(chipherTxt));
+        }
+    }
+}
